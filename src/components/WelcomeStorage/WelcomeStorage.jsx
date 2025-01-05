461fdb9a67d6b3c43a214845646e03202b0b40e2
Vladyslav D <donetsvlad444@gmail.com>
2025-01-05 17:38:05 +0200

    fixed dcument

--- src/components/WelcomeStorage/WelcomeStorage.jsx
+++ src/components/WelcomeStorage/WelcomeStorage.jsx
@@ -68,18 +68,22 @@ const WelcomeStorage = ({ title, buttonStyle, calc_id, info, backImage }) => {
           data-aos-offset="0"
           data-aos-duration="1500"
         >
-          <p>
-            {t(info)}{" "}
-            {
-              <a
-                href="https://docs.google.com/document/d/1CUcEp5vyPVeNSXUfGjDp2ew_V31RybuT04vuxdy5kFk/edit?tab=t.0"
-                target="_blank"
-                rel="noreferrer"
-              >
-                {t("public_document")}
-              </a>
-            }
-          </p>
+          {buttonStyle ? (
+            <p>{t(info)}</p>
+          ) : (
+            <p>
+              {t(info)}{" "}
+              {
+                <a
+                  href="https://docs.google.com/document/d/1CUcEp5vyPVeNSXUfGjDp2ew_V31RybuT04vuxdy5kFk/edit?tab=t.0"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  {t("public_document")}
+                </a>
+              }
+            </p>
+          )}
         </div>
       </article>
     </section>
